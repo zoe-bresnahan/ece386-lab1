260376aac8b89419397774921133cbb931846ac1
byarbrough <6315292+byarbrough@users.noreply.github.com>
2025-01-09 22:57:14 -0700

    Open bytes as pillow image

--- server/digits.py
+++ server/digits.py
@@ -3,8 +3,22 @@ TODO: Insert what this program does here. Should start with
 digits is a FastAPI app that...
 """
 
-# TODO: Open saved Keras model as global variable
+from PIL import Image
+from io import BytesIO
+
+model_path: str = "digits.keras"
+# TODO: Open saved Keras model as global variable. NO TYPE HINT REQUIRED!
 
 # TODO: Create FastAPI App as global variable
+
+
+def image_to_np(image_bytes: bytes) -> np.ndarray:
+    """Convert image to proper numpy array"""
+    # First must use pillow to process bytes
+    img = Image.open(BytesIO(image_bytes))
+    # TODO: convert image to grayscale and resize
+    # TODO: convert image to numpy array of shape model expects
+    return None
+
 
 # TODO: Define predict POST function
