11cb7b56d73a68702dbcf2bac4db825c61d29e26
byarbrough <6315292+byarbrough@users.noreply.github.com>
2025-01-09 17:10:05 -0700

    Add TODO stubs

---
+++ client/client.py
@@ -0,0 +1,32 @@
+"""
+TODO: Insert what this program does here.
+"""
+
+import sys
+
+
+# TODO: Decide if str is the best return type, or maybe change it?
+def get_img_prediction(
+    server_ip: str, server_port: int, api_path: str, image_path: str
+) -> str:
+    """Send image to server for prediction."""
+    # TODO: Replace with code to send image to server
+    return ""
+
+
+def main(server_ip: str, server_port: int) -> None:
+    """Repeatedly prompt the user for a path to an image
+    and send it to the server for prediction.
+    Then display the result to the user.
+    """
+    # TODO: Replace with prompt to user and call to get_img_prediction
+    print(f"Got arguments: {server_ip}, {server_port}")
+
+
+if __name__ == "__main__":
+    # Ensure user passes required arguments
+    if len(sys.argv) != 3:
+        print("Usage: python client.py <server IP address> <server port>")
+        sys.exit(1)
+
+    main(sys.argv[1], int(sys.argv[2]))
---
+++ server/digits.py
@@ -0,0 +1,10 @@
+"""
+TODO: Insert what this program does here. Should start with
+digits is a FastAPI app that...
+"""
+
+# TODO: Open saved Keras model as global variable
+
+# TODO: Create FastAPI App as global variable
+
+# TODO: Define predict POST function
